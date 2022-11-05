25e6a4a101394db641db3fa316a63ca701ece27b
Arjun Tumuluri <arjuntumuluri@gmail.com>
2022-11-04 19:28:28 -0500

    Rudimentary Forecasting to test out prophet

---
+++ ProphetForecast.py
@@ -0,0 +1,50 @@
+#install prophet and yfinance
+#in command line: python - m install prophet
+#in command line: pip install yfinance
+
+from datetime import date
+import yfinance as yf
+from prophet import Prophet
+
+a 
+#Get Data
+START = "2017-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+
+#Select Stock to analyze
+selected_stock = "META"
+
+data = yf.download(selected_stock, START, TODAY)
+data.reset_index(inplace = True)
+
+print(data.tail())
+
+#Predict Forecast with Prophet
+
+df_train = data[["Date", "Close"]]
+df_train = df_train.rename(columns ={"Date":"ds", "Close":"y"})
+
+m = Prophet()
+m.fit(df_train)
+
+#Create Future Period to Predict
+n_years = 1
+period = n_years * 365
+future = m.make_future_dataframe(periods = period)
+forecast = m.predict(future)
+
+forecast[["ds", "yhat", "yhat_lower", "yhat_upper"]].tail()
+print(forecast[["ds", "yhat", "yhat_lower", "yhat_upper"]].tail())
+
+#plot forecast
+figure1 = m.plot(forecast)
+
+#plot trend, weekly, yearly forecast
+
+figure2 = m.plot_components(forecast)
+
+figure1.show()
+figure2.show()
+
+#Input to close, otherwise graphs disappear instantly
+input("Press y to close: ")
